893e03a26bfa04dd140954ebfa3a5dd58db4b65d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-29 17:50:05 -0700

    cleaning up code

--- tests/utils/messageDecoder.test.js
+++ tests/utils/messageDecoder.test.js
@@ -1,17 +1,14 @@
 const { messageDecoder } = require('../../lib/utils/messageDecoder');
 
 describe('message decoder tests', () => {
-  let body = null;
   it('concats chunks into one body', () => {
     const chunk1 = '!01!chunk1';
     const chunk2 = 'chunk2';
     const chunk3 = 'chunk3%01%';
-    let fullData = null;
     const decoder = messageDecoder();
     decoder(chunk1);
     decoder(chunk2);
     const result = decoder(chunk3);
-    console.log(body, 'body', fullData, 'fullData');
     expect(result).toEqual(chunk1 + chunk2 + chunk3);
   });
 });
