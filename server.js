ada3a696287b5d5079102ba28c86f83b6c55165e
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-01 11:15:06 -0700

    connected to mongoose db added .env

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+require('dotenv').config();
 const app = require('./lib/app');
+const mongoose = require('mongoose');
 
-app.listen(54321);
-
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+app.listen(54321, () => {
+  console.log('You are listening to smooooth jazz on `54321`');
+});
 
